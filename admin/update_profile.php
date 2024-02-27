e3a92e94d5782e2eeaa21da45586a32e2ee4c8e6
mrkkjfrsn <151515845+mrkkjfrsn@users.noreply.github.com>
2024-02-27 22:36:32 +0800

    changes

--- admin/update_profile.php
+++ admin/update_profile.php
@@ -70,7 +70,7 @@ if (isset($_POST['submit'])) {
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!-- custome css  -->
-  <link rel="stylesheet" href="../css/admin_style/general.css">
+  <link rel="stylesheet" href="../css/admin_style/general_admin.css">
    <link rel="stylesheet" href="../css/admin_style/accounts.css">
    <link rel="stylesheet" href="../css/admin_style/admin_header.css">
    <link rel="stylesheet" href="../css/admin_style/buttons.css">
